7ec9a94a1dfdb65330da36988ba306c2bb844fe5
vincent <100250184+vguzman812@users.noreply.github.com>
2024-02-22 14:35:13 -0500

    landing page done

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,83 @@
+import * as React from "react";
+import { alpha } from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+
+export default function Hero() {
+  return (
+    <Box
+      id="hero"
+      sx={() => ({
+        width: "100%",
+        backgroundImage: "linear-gradient(180deg, #CEE5FD, #FFF)",
+        backgroundSize: "100% 60%",
+        backgroundRepeat: "no-repeat",
+      })}
+    >
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          pt: { xs: 14, sm: 20 },
+          pb: { xs: 8, sm: 12 },
+        }}
+      >
+        <Stack
+          spacing={0.5}
+          useFlexGap
+          sx={{ width: { xs: "100%", sm: "70%" } }}
+        >
+          <Typography
+            component="h1"
+            variant="h1"
+            sx={{
+              display: "flex",
+              flexDirection: { xs: "column", md: "row" },
+              alignSelf: "center",
+              textAlign: "center",
+
+            }}
+          >
+            Welcome to &nbsp;
+            <Typography
+              component="span"
+              variant="h1"
+              sx={{
+                color: "primary.main",
+              }}
+            >
+              AirMap
+            </Typography>
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            textAlign="center"
+            color="text.secondary"
+            sx={{
+              mt: { s: 10, md: -5 },
+            }}
+          >
+            A platform for visualizing real-time flight data.
+          </Typography>
+          <Stack
+            direction={{ xs: "column", sm: "row" }}
+            alignSelf="center"
+            spacing={1}
+            useFlexGap
+            sx={{ pt: 2, width: { xs: "100%", sm: "auto" } }}
+          >
+            <Button variant="contained" color="primary">
+              See the Map!
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,18 @@
-import { Typography, Stack, Container } from "@mui/material";
-import Box from "@mui/material/Box";
+import { Typography, Stack, Box } from "@mui/material";
+
 import TemplateTester from "@/components/TemplateTester/TemplateTester";
 import Footer from "@/components/Footer/Footer";
 import NavBar from "@/components/NavBar/NavBar";
+import Hero from "@/components/Hero/Hero";
 
 function Home() {
   return (
-    <Container sx={{ py: 2, position: "relative" }}>
-      <Stack gap={1} my={2}>
-        <NavBar />
-        <Typography textAlign="center" variant="subtitle1">
-          React + Redux + MuI + Axios + ESlint + Prettier
-        </Typography>
-      </Stack>
-      <TemplateTester />
+    <Box sx={{ position: "relative" }}>
+      <NavBar />
+      <Hero />
       <Footer />
-    </Container>
+      {/* <TemplateTester /> */}
+    </Box>
   );
 }
 
