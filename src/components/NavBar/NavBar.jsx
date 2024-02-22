129917bac4c1fac30ba66ab39b0cb93c65200343
vincent <100250184+vguzman812@users.noreply.github.com>
2024-02-22 14:09:45 -0500

    navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,129 @@
+import * as React from "react";
+
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import MenuItem from "@mui/material/MenuItem";
+import Drawer from "@mui/material/Drawer";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const logoStyle = {
+  width: "40px",
+  height: "auto",
+  cursor: "pointer",
+};
+
+function NavBar() {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  return (
+    <div>
+      <AppBar
+        position="fixed"
+        sx={{
+          boxShadow: 0,
+          bgcolor: "transparent",
+          backgroundImage: "none",
+          mt: 2,
+        }}
+      >
+        <Container maxWidth="lg">
+          <Toolbar
+            variant="regular"
+            sx={() => ({
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              flexShrink: 0,
+              borderRadius: "999px",
+              bgcolor: "rgba(255, 255, 255, 0.4)",
+              backdropFilter: "blur(24px)",
+              maxHeight: 40,
+              border: "1px solid",
+              borderColor: "divider",
+              boxShadow: `0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)`,
+            })}
+          >
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: "flex",
+                alignItems: "center",
+                ml: "-10px",
+                px: 0,
+              }}
+            >
+              <img src="/logo.png" style={logoStyle} alt="logo of AirMap" />
+            </Box>
+            <Box
+              sx={{
+                display: { xs: "none", md: "flex" },
+                gap: 0.5,
+                alignItems: "center",
+              }}
+            >
+              <Button
+                color="primary"
+                variant="contained"
+                size="small"
+                component="a"
+                href="/map"
+              >
+                The Map
+              </Button>
+            </Box>
+            <Box sx={{ display: { sm: "", md: "none" } }}>
+              <Button
+                variant="text"
+                color="primary"
+                aria-label="menu"
+                onClick={toggleDrawer(true)}
+                sx={{ minWidth: "30px", p: "4px" }}
+              >
+                <MenuIcon />
+              </Button>
+              <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
+                <Box
+                  sx={{
+                    minWidth: "60dvw",
+                    p: 2,
+                    backgroundColor: "background.paper",
+                    flexGrow: 1,
+                  }}
+                >
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "end",
+                      flexGrow: 1,
+                    }}
+                  />
+                  <MenuItem>
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      component="a"
+                      href="/map"
+                      sx={{ width: "100%" }}
+                    >
+                      The Map
+                    </Button>
+                  </MenuItem>
+                </Box>
+              </Drawer>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+  );
+}
+
+export default NavBar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,13 @@ import { Typography, Stack, Container } from "@mui/material";
 import Box from "@mui/material/Box";
 import TemplateTester from "@/components/TemplateTester/TemplateTester";
 import Footer from "@/components/Footer/Footer";
+import NavBar from "@/components/NavBar/NavBar";
 
 function Home() {
   return (
     <Container sx={{ py: 2, position: "relative" }}>
       <Stack gap={1} my={2}>
-        <Typography textAlign="center" variant="h2">
-          Viterjs-template
-        </Typography>
+        <NavBar />
         <Typography textAlign="center" variant="subtitle1">
           React + Redux + MuI + Axios + ESlint + Prettier
         </Typography>
