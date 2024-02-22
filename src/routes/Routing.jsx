9e0e23917069a332556ae5b4cd6ef14b64389e9c
vincent <100250184+vguzman812@users.noreply.github.com>
2024-02-22 13:30:15 -0500

    favicon

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home/Home";
+import Map from "@/pages/Map/Map";
 
 function Routing() {
   return (
     <Routes>
       <Route path="*" element={<Home />} />
-      {/* <Route
-        path='/login/*'
-        element={<Login />}
-      /> */}
+      <Route path="/map/*" element={<Map />} />
     </Routes>
   );
 }
